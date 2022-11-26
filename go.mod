4870f7143bbeb69ff4eaa18aa2c3dd050564d5a9
Olivier Hoareau <oha@greenberets.io>
2022-11-26 19:54:03 +0100

    feat: add sentry dependency

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ require github.com/genstackio/gogh v0.4.0
 
 require (
 	github.com/aws/aws-lambda-go v1.26.0 // indirect
+	github.com/getsentry/sentry-go v0.15.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
 
 go 1.17
--- sentry.go
+++ sentry.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"github.com/genstackio/gogh"
 	"github.com/genstackio/gogh/common"
+	"github.com/getsentry/sentry-go"
 	"log"
+	"os"
 )
 
 type Provider struct {
@@ -16,35 +18,44 @@ func (p *Provider) AddCaptureContext(ctx common.CaptureContext) {
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureError(err error, ctx common.CaptureContext) {
+	sentry.CaptureException(err)
 }
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureMessage(message string, ctx common.CaptureContext) {
-	log.Println("BLABLA", message)
+	sentry.CaptureMessage(message)
 }
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureMessages(messages []string, ctx common.CaptureContext) {
+	for i := 0; i < len(messages); i++ {
+		sentry.CaptureMessage(messages[i])
+	}
 }
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureProperty(typ string, data interface{}, ctx common.CaptureContext) {
+	// @todo
 }
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureData(bulkData map[string]interface{}, ctx common.CaptureContext) {
+	// @todo
 }
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureEvent(event interface{}, ctx common.CaptureContext) {
+	// @todo
 }
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureTag(tag string, value interface{}, ctx common.CaptureContext) {
+	// @todo
 }
 
 //goland:noinspection GoUnusedParameter
 func (p *Provider) CaptureTags(tags map[string]interface{}, ctx common.CaptureContext) {
+	// @todo
 }
 
 //goland:noinspection GoUnusedParameter
@@ -61,6 +72,13 @@ func (p *Provider) Wrap(h common.HandlerFn) common.HandlerFn {
 
 //goland:noinspection GoUnusedExportedFunction
 func Create() common.Provider {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              os.Getenv("SENTRY_DSN"),
+		TracesSampleRate: 1.0,
+	})
+	if err != nil {
+		log.Println("gogh-sentry: unable to initialize sentry: " + err.Error())
+	}
 	return &Provider{}
 }
 

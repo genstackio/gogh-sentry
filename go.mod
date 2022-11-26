8d4b0995cd5ebc02932defc18db72acc90983929
Olivier Hoareau <oha@greenberets.io>
2022-11-26 19:28:59 +0100

    chore: creation

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/genstackio/gogh-sentry
+
+require github.com/genstackio/gogh v0.4.0
+
+require (
+	github.com/aws/aws-lambda-go v1.26.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+)
+
+go 1.17
---
+++ sentry.go
@@ -0,0 +1,64 @@
+package none
+
+import (
+	"context"
+	"github.com/genstackio/gogh/common"
+	"log"
+)
+
+type Provider struct {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) AddCaptureContext(ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureError(err error, ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureMessage(message string, ctx common.CaptureContext) {
+	log.Println("BLABLA", message)
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureMessages(messages []string, ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureProperty(typ string, data interface{}, ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureData(bulkData map[string]interface{}, ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureEvent(event interface{}, ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureTag(tag string, value interface{}, ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) CaptureTags(tags map[string]interface{}, ctx common.CaptureContext) {
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) Error(err error, ctx common.CaptureContext) {
+	p.CaptureError(err, ctx)
+}
+
+//goland:noinspection GoUnusedParameter
+func (p *Provider) Wrap(h common.HandlerFn) common.HandlerFn {
+	return func(ctx context.Context, payload []byte) ([]byte, error) {
+		return h(ctx, payload)
+	}
+}
+
+//goland:noinspection GoUnusedExportedFunction
+func Create() common.Provider {
+	return &Provider{}
+}
